fb97697696dc6484299e6a667b31a27a7c48f692
eduardo salinas <eduardosalinas@MacBook-Pro.local>
2022-12-19 21:39:31 -0300

    Commit

--- JavaScript/pruebas/primos.js
+++ JavaScript/pruebas/primos.js
@@ -1,15 +1,13 @@
 function primos(array) {
-    console.log("Funciona")
     for (let i = 0; i < array.lenght; i++) {
         let cont = 0
         for (let j = 0; j < array[i]; j++) {
+            console.log("hola")
             if (array[i] % j == 0) {
-                cont++
-            }
+                cont++}
         if (cont > 2) {
             console.log(array[i] + ": Es un numero primo")
-            return primos(array);
-        }
+            return primos(array);}
         else{
             console.log(array[i] + ": No es un numero primo")
             return primos(array);
