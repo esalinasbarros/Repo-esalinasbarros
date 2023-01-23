89a54aa4d5783439f1832e7c82883f80f3188942
eduardo salinas <eduardosalinas@MacBook-Pro.local>
2023-01-23 20:01:39 -0300

    Commit

---
+++ C++/funciones/while_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+/* Primero en pyhton */
+int main() {
+    int contador = 0;
+    do
+    {
+        cout << contador;
+        cout << '\n';
+        contador++;   
+    } while (contador < 1);
+    return 0;
+};
+
+
+
---
+++ Proyectos/spam/spam.py
@@ -0,0 +1,12 @@
+import pyautogui as pg
+from time import sleep
+sleep(4)
+def wrte():
+    for _ in range(3000):
+        pg.write('Oe')
+        pg.press('Enter')
+        pg.write('Cuando veni a ranco???')
+        pg.press('Enter')
+escribir = wrte()
+
+
