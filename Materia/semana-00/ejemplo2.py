d5d7b3e8121a387fdee14d09bd96b0f57bff4dd5
esalinasbarros <esalinasbarros@uc.cl>
2023-08-01 22:32:52 -0400

    actualizacion

--- Materia/semana-00/ejemplo2.py
+++
@@ -1,5 +0,0 @@
-
-mi_nombre = __name__
-
-if __name__ == "__main__":
-    print("Soy el módulo principal")
--- Materia/semana-00/ejemplo4/main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-if __name__ == '__main__':
-    print("Me estoy ejecutando desde:")
-    path = os.getcwd()
-    print(path)
-    content = open("hello world.txt").read()
-    print(content)
